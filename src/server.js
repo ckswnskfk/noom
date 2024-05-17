28365ff05e2f6c63bec74dbe244103bdbc5df291
ckswnskfk <ckswnskfk@hanmail.net>
2024-05-17 18:03:45 +0900

    feat: admin panel 설치 및 기본 설정

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -13,7 +14,17 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wsServer = new Server(server);
+const wsServer = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+  mode: "development",
+});
 
 function getPublicRooms() {
   const {
